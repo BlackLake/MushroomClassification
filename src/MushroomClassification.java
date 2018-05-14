9a7e06cf4a7ad59c60b20a55aa3eaf7267fc67d2
blacklake <oguzhankaragol60@gmail.com>
2018-05-14 16:45:40 +0300

    number of edibles classified as poisonous and number of poisonous classified as edibles Counters added

--- src/MushroomClassification.java
+++ src/MushroomClassification.java
@@ -5,16 +5,22 @@ import java.util.Arrays;
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.learning.DataSet;
 import org.neuroph.core.learning.DataSetRow;
+import org.neuroph.nnet.Kohonen;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.learning.MomentumBackpropagation;
 import org.neuroph.util.TrainingSetImport;
 import org.neuroph.util.TransferFunctionType;
+
+import javax.xml.crypto.Data;
 
 
 public class MushroomClassification {
 
     static double learningRate = 0.7;
     static double momentum = 0.4;
+
+    static int numberOfEdiblesClassifiedAsPoisonous=0;
+    static int numberOfPoisonousClassifiedAsEdible=0;
 
 
     public static void main(String[] args) {
@@ -25,6 +31,8 @@ public class MushroomClassification {
 
         String trainingSetFileName = "training.data";
         String testingSetFileName = "test.data";
+        int kohonenInputsCount = 126;
+        int kohonenOutputsCount = 15;
         int inputsCount = 126;
         int outputsCount = 2;
 
@@ -41,6 +49,21 @@ public class MushroomClassification {
         } catch (IOException | NumberFormatException ex) {
             System.out.println("Error reading file or bad number format!");
         }
+/*
+        // create kohonen set
+        DataSet kohonenSet = null;
+        try {
+            kohonenSet = TrainingSetImport.importFromFile(trainingSetFileName, kohonenInputsCount, kohonenOutputsCount, ",");
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found!");
+        } catch (IOException | NumberFormatException ex) {
+            System.out.println("Error reading file or bad number format!");
+        }
+
+        // create kohonen network
+        NeuralNetwork kohonenNeuralNetwork = new Kohonen(kohonenInputsCount,kohonenOutputsCount);
+        kohonenNeuralNetwork.learn(kohonenSet);*/
+
 
         // create multi layer perceptron
         System.out.println("Creating neural network");
@@ -72,6 +95,23 @@ public class MushroomClassification {
         testClassificationOfMushroomsPoisonous(neuralNet, testingSet);
 
     }
+/*
+    public static ArrayList<double[]> testKohonen(NeuralNetwork nnet, DataSet dset) {
+        ArrayList<double[]> kohonenOutputs = new ArrayList<>();
+        for (DataSetRow trainingElement : dset.getRows()) {
+
+            nnet.setInput(trainingElement.getInput());
+            nnet.calculate();
+            double[] networkOutput = nnet.getOutput();
+            kohonenOutputs.add(networkOutput);
+            System.out.print("Input: " + Arrays.toString(trainingElement.getInput()));
+            System.out.println(" Output: " + Arrays.toString(networkOutput));
+
+            //DataSet dataSet = null;
+            //dataSet = TrainingSetImport.importFromFile();
+        }
+        return kohonenOutputs;
+    }*/
 
     public static void testClassificationOfMushroomsPoisonous(NeuralNetwork nnet, DataSet dset) {
 
@@ -98,7 +138,7 @@ public class MushroomClassification {
 
                 String row = dis.readLine();
                 String[] elements = row.split(",");
-                int[] temp = {Integer.parseInt(elements[126]),Integer.parseInt(elements[127])};
+                int[] temp = {Integer.parseInt(elements[126]), Integer.parseInt(elements[127])};
                 givenTestOutputs.add(temp);
 
             }
@@ -134,19 +174,29 @@ public class MushroomClassification {
             //System.out.print("Input: " + Arrays.toString(trainingElement.getInput()));
             //System.out.println(" Output: " + Arrays.toString(networkOutput));
 
-            if (networkOutput[0] == givenTestOutputs.get(totalCounter)[0] && networkOutput[1] == givenTestOutputs.get(totalCounter)[1])
-            {
+            if (networkOutput[0] == givenTestOutputs.get(totalCounter)[0] && networkOutput[1] == givenTestOutputs.get(totalCounter)[1]) {
                 trueCounter++;
-
+                if (networkOutput[0] == 0 && networkOutput[1] == 1) {
+                    System.out.println("mushroom number " + totalCounter + " is edible");
+                } else {
+                    System.out.println("mushroom number " + totalCounter + " is posionus");
+                }
+            } else if (networkOutput[0] == 0 && networkOutput[1] == 1) {
+                numberOfPoisonousClassifiedAsEdible++;
+            } else if (networkOutput[0] == 1 && networkOutput[1] == 0) {
+                numberOfEdiblesClassifiedAsPoisonous++;
             }
             totalCounter++;
 
+
         }
         System.out.println();
-        System.out.println("             FINAL RESULTS");
-        System.out.println("Correctly predicted instances  :  " +trueCounter);
-        System.out.println("Total test instances           :  " +totalCounter);
-        System.out.println("Percentage : " + ((double)trueCounter/totalCounter)*100 + " %");
+        System.out.println("                       FINAL RESULTS");
+        System.out.println("Correctly predicted instances               :  " + trueCounter);
+        System.out.println("Total test instances                        :  " + totalCounter);
+        System.out.println("Number of poisonous classified as edible    :  " + numberOfPoisonousClassifiedAsEdible);
+        System.out.println("Number of edibles classified as poisonous   :  " + numberOfEdiblesClassifiedAsPoisonous);
+        System.out.println("Percentage : " + ((double) trueCounter / totalCounter) * 100 + " %");
     }
 
 
